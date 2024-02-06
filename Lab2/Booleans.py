e95dd6698134ddfd7ccac375e598baa4b8dd61de
Vlad-Jan <158034547+Vlad-Jan@users.noreply.github.com>
2024-02-06 13:38:17 +0600

    Add files via upload

---
+++ Lab2/Booleans.py
@@ -0,0 +1,19 @@
+# Exercise 1
+# print (10>9)
+# it will show TRUE 
+
+# Exercise 2
+# print(10 == 9)
+# it will show FALSE
+
+# Exercise 3
+# print(10 < 9)
+# it will show FALSE
+
+# Exercise 4
+# print(bool("abc"))
+# it will show TRUE
+
+# Exercise 5
+# print(bool(0))
+# it will show FALSE
---
+++ Lab2/Dictionaries.py
@@ -0,0 +1,46 @@
+# Exercise 1
+""" car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))""" 
+
+# Exercise 2
+
+""" car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+print (car.get("year")) """
+
+# Exercise 3
+
+""" car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+print(car.get("color")) """ 
+
+# Exercise 4
+""" car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")"""
+
+# Exercise 5
+""" car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()""" 
+
+
+
